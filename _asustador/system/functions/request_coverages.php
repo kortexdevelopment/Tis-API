1ecf0bc0eab3638154cde9ddae16cf8abc11c002
unknown <carlos.reyes.llamas.2008@gmail.com>
2021-07-14 12:39:26 -0700

    fix directories

--- _asustador/system/functions/request_coverages.php
+++
@@ -1,9 +0,0 @@
-<?php
-	require_once $_SERVER["DOCUMENT_ROOT"] . "/system/functions/db_config.php";
-	
-	$sql = "SELECT * FROM request_coverage WHERE doc_request_id = $request_id";
-	
-	$query_result = $db_conn->query($sql);
-	
-	$request_coverages = $query_result->fetch_array(MYSQLI_NUM);
-?>
